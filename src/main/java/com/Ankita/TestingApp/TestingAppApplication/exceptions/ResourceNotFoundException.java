a109610125f3e8f41ede08bb21692ee17e245a3b
Ankita Pandey <ankitapandey1576@gmail.com>
2026-07-20 21:29:09 +0530

    initial project structure

--- src/main/java/com/Ankita/TestingApp/TestingAppApplication/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/Ankita/TestingApp/TestingAppApplication/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package com.Ankita.TestingApp.TestingAppApplication.exceptions;
 
-public class ResourceNotFoundException extends RuntimeException{
+public class  ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message) {
         super(message);
     }
